2e053acab6b1ccbd71620722df970b8e0bba572b
Louis <louis-kaan2002@web.de>
2022-06-27 19:42:57 +0200

    added prev array to dijkstra

--- src/graphalgorithms/DijkstraAdjacencyMatrix.java
+++ src/graphalgorithms/DijkstraAdjacencyMatrix.java
@@ -2,35 +2,109 @@ package graphalgorithms;
 
 import datastructures.graphs.AdjacencyMatrix;
 
-public class Dijkstra {
+import java.util.Arrays;
 
-    private int minDistance(int dist[], boolean[] shortestpath){
+public class DijkstraAdjacencyMatrix {
 
-        return Integer.MAX_VALUE;
+    private int getMinDistance(int[] distance, boolean[] shortestPathTree) {
+
+        // to store smallest distance
+        int minimum = Integer.MAX_VALUE;
+
+        // index of the vertex
+        int vertex = -1;
+
+        for (int i = 0; i < distance.length; i++) {
+            if (!shortestPathTree[i] && distance[i] <= minimum) {
+                minimum = distance[i];
+                vertex = i;
+            }
+        }
+
+        return vertex;
+    }
+
+    public void dijkstra(int[][] graph, int startVertex) {
+
+        // store the number of vetices in Graph
+        int numberOfVertices = graph.length;
+
+        boolean[] shortestPathTree = new boolean[numberOfVertices];
+        int[] distance = new int[numberOfVertices];
+        Integer[] prev = new Integer[numberOfVertices];
+
+
+        // initialize all distances with infinity = Integer.Max_Value
+        for (int i = 0; i < numberOfVertices; i++) {
+            distance[i] = Integer.MAX_VALUE;
+        }
+
+        // set start Vertex distance to 0, bc we start from this vertex so
+        distance[startVertex] = 0;
+
+        for (int i = 0; i < numberOfVertices; i++) {
+
+            int vertex = getMinDistance(distance, shortestPathTree);
+
+            shortestPathTree[vertex] = true;
+
+            // iterate over all edges of this vertex
+            for (int j = 0; j < numberOfVertices; j++) {
+
+                // if greater than 0, then there is an edge
+                if (graph[vertex][j] > 0) {
+
+                    if (!shortestPathTree[j] && distance[vertex] != Integer.MAX_VALUE) {
+
+                        int newDistance = distance[vertex] + graph[vertex][j];
+
+                        if (newDistance < distance[j])
+                            distance[j] = distance[vertex] + graph[vertex][j];
+                            prev[j] = vertex;
+                    }
+                }
+
+            }
+        }
+
+        System.out.println(Arrays.toString(prev));
+        printShortestPath(startVertex, distance);
+    }
+
+    private void printShortestPath(int startVertex, int[] distance) {
+        for (int i = 0; i < distance.length; i++) {
+            System.out.println("Source Vertex: " + startVertex + " to vertex " + i +
+                    " distance: " + distance[i]);
+        }
     }
 
     public static void main(String[] args) {
 
+        DijkstraAdjacencyMatrix dikstra = new DijkstraAdjacencyMatrix();
+
         AdjacencyMatrix graph = new AdjacencyMatrix(8);
 
-        graph.addEdge(0,1, 2);
-        graph.addEdge(0,2, 10);
-        graph.addEdge(0,3, 5);
-        graph.addEdge(0,4, 8);
-        graph.addEdge(1,2, 8);
-        graph.addEdge(1,4, 16);
-        graph.addEdge(1,5, 6);
-        graph.addEdge(1,6, 15);
-        graph.addEdge(2,3, 4);
-        graph.addEdge(2,4, 7);
-        graph.addEdge(2,6, 12);
-        graph.addEdge(3,4, 10);
-        graph.addEdge(5,6, 20);
-        graph.addEdge(5,7, 4);
-        graph.addEdge(6,7, 8);
+        graph.addEdge(0, 1, 2);
+        graph.addEdge(0, 2, 10);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 8);
+        graph.addEdge(1, 2, 8);
+        graph.addEdge(1, 4, 16);
+        graph.addEdge(1, 5, 6);
+        graph.addEdge(1, 6, 15);
+        graph.addEdge(2, 3, 4);
+        graph.addEdge(2, 4, 7);
+        graph.addEdge(2, 6, 12);
+        graph.addEdge(3, 4, 10);
+        graph.addEdge(5, 6, 20);
+        graph.addEdge(5, 7, 4);
+        graph.addEdge(6, 7, 8);
 
         graph.print();
 
+        System.out.println();
+
+        dikstra.dijkstra(graph.getGraph(), 0);
 
 
     }
