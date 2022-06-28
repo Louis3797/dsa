e7ecb478dd4d91e62a29e832dce204358cfde8d7
Louis <louis-kaan2002@web.de>
2022-06-28 14:50:25 +0200

    implemented bfs algo for adjacency matrix and list representation

--- src/graphalgorithms/BfsAdjacencyList.java
+++ src/graphalgorithms/BfsAdjacencyList.java
@@ -1,2 +1,56 @@
-package graphalgorithms;public class BfsAdjacencyList {
+package graphalgorithms;
+
+import datastructures.graphs.Graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BfsAdjacencyList {
+
+    public void bfs(Graph<Integer> graph, int startVertex) {
+
+        int len = graph.getNumberOfVertices();
+
+        boolean visited[] = new boolean[len];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+
+            int vertex = queue.poll();
+
+            System.out.println(vertex + " ");
+
+            for (Graph<Integer>.Edge e : graph.getGraph().get(vertex)) {
+
+                int destination = e.getDestination();
+
+                if (!visited[destination]) {
+                    visited[destination] = true;
+                    queue.add(destination);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BfsAdjacencyList bfs = new BfsAdjacencyList();
+        Graph<Integer> graph = new Graph<>(false);
+
+        graph.addEdge(0, 2, 1);
+        graph.addEdge(0, 1, 1);
+        graph.addEdge(1, 2, 1);
+        graph.addEdge(2, 0, 1);
+        graph.addEdge(2, 3, 1);
+        graph.addEdge(3, 3, 1);
+
+        graph.print();
+        System.out.println();
+        bfs.bfs(graph, 2);
+    }
+
 }
--- src/graphalgorithms/BfsAdjacencyMatrix.java
+++ src/graphalgorithms/BfsAdjacencyMatrix.java
@@ -1,2 +1,52 @@
-package graphalgorithms;public class BfsAdjacencyMatrix {
+package graphalgorithms;
+
+import datastructures.graphs.AdjacencyMatrix;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BfsAdjacencyMatrix {
+
+    public void bfs(AdjacencyMatrix graph, int startVertex) {
+
+        int len = graph.size();
+
+        boolean visited[] = new boolean[len];
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        while (!queue.isEmpty()) {
+
+            int vertex = queue.poll();
+
+            System.out.println(vertex + " ");
+
+            for (int v : graph.neighbours(vertex)) {
+
+                if (!visited[v]) {
+                    visited[v] = true;
+                    queue.add(v);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BfsAdjacencyMatrix bfs = new BfsAdjacencyMatrix();
+        AdjacencyMatrix graph = new AdjacencyMatrix(4);
+
+        graph.addEdge(0, 2, 1);
+        graph.addEdge(0, 1, 1);
+        graph.addEdge(1, 2, 1);
+        graph.addEdge(2, 3, 1);
+        graph.addEdge(3, 3, 1);
+
+        graph.print();
+        System.out.println();
+        bfs.bfs(graph, 2);
+    }
 }
